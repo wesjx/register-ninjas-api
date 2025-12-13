9dd93d62d99ce43254f3d6a6afc6dc9a50ffb803
Wesley Silva <wesleysilva@Wesleys-MacBook-Pro.local>
2025-12-13 13:34:23 +0000

    fix: enforce unique email in ninjas table

--- src/main/java/dev/wesleyjunior/RegisterNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/wesleyjunior/RegisterNinjas/Ninjas/NinjaModel.java
@@ -6,30 +6,28 @@ import lombok.*;
 
 import java.util.UUID;
 
-//Lobok depedence
+//Lombok dependency
 @NoArgsConstructor
 @AllArgsConstructor
 @Data //Getters and setters
 
 @Entity
 @Table(name = "tb_register_ninjas")
+
 public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
+
+    @Column(unique = true) //Column is unique, for sensitives datas
     private String email;
+
     private int age;
 
     @ManyToOne
     @JoinColumn(name = "missions_id") // Foreing Key
     private MissionsModel missions;
-
-    public NinjaModel(String name, String email, int age) {
-        this.name = name;
-        this.email = email;
-        this.age = age;
-    }
 
 }
