04e02aea5ec5bc5ba59de31991a6343b1b4fa9f1
Wesley Junio <wesjrpiri@gmail.com>
2026-01-09 18:57:46 +0000

    fix: includes column of rank in the entity but it was in migration V2.

--- src/main/java/dev/wesleyjunior/RegisterNinjas/Ninjas/NinjaDTO.java
+++ src/main/java/dev/wesleyjunior/RegisterNinjas/Ninjas/NinjaDTO.java
@@ -1,4 +1,27 @@
 package dev.wesleyjunior.RegisterNinjas.Ninjas;
 
+import dev.wesleyjunior.RegisterNinjas.Missions.MissionsModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaDTO {
+
+    private UUID id;
+
+    private String name;
+
+    private String email;
+
+    private int age;
+
+    private MissionsModel missions;
+
+    private String Rank;
 }
