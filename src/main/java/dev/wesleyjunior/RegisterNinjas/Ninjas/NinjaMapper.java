0c924782c131d215049ec3002b4130611234deee
Wesley Junio <wesjrpiri@gmail.com>
2026-01-09 19:00:27 +0000

    Chore: includes version of DTO by entity and mapping manually in NinjaMapper the ways Entity x DTO.

--- src/main/java/dev/wesleyjunior/RegisterNinjas/Ninjas/NinjaMapper.java
+++ src/main/java/dev/wesleyjunior/RegisterNinjas/Ninjas/NinjaMapper.java
@@ -1,4 +1,36 @@
 package dev.wesleyjunior.RegisterNinjas.Ninjas;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class NinjaMapper {
+
+    public NinjaModel map(NinjaDTO ninjaDTO) {
+
+        NinjaModel ninjaModel = new NinjaModel();
+
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setName(ninjaDTO.getName());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setAge(ninjaDTO.getAge());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setMissions(ninjaDTO.getMissions());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel) {
+        NinjaDTO ninjaDTO = new NinjaDTO();
+
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setName(ninjaModel.getName());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setAge(ninjaModel.getAge());
+        ninjaDTO.setRank(ninjaModel.getRank());
+        ninjaDTO.setMissions(ninjaModel.getMissions());
+
+        return ninjaDTO;
+    }
+
+
 }
